74a26c1728a70553fe2cb30f47bd4a263c795bb8
Nilson-N-Nascimento <negreirosnilson@outlook.com>
2024-08-28 12:59:59 +0000

    add: ATIVIDADE

---
+++ balancotrimestral.java
@@ -0,0 +1,14 @@
+class balancotrimestral {
+        public static void main(String[] args) {
+        //atividade01
+            int gastosjaneiro = 15000;
+            int gastosfevereiro = 23000;
+            int gastosmarco = 17000;
+
+            int gastotrimestre = gastosmarco + gastosfevereiro + gastosjaneiro;
+
+            System.out.println(gastotrimestre);
+
+    }
+
+}
--- testeidade.java
+++ testeidade.java
@@ -1,15 +1,32 @@
 class TesteIdade {
     public static void main(String[] args) {
         int idade;
-        idade = 15;
+        idade = 20;
         
         int idadeAnoQueVem = idade + 1;
         int resultado = idade % 2;
         double MetadeIdade = idade / 2;
 
+        //if (idade >= 18);
+        // println("Pode entrar");
+        // else:
+        boolean maioridade;
+        maioridade = idade >= 18;
+        
+        
+        boolean TipoSangue;
+        TipoSangue = 'a' == 'A';
+        int i =  5;
+        int j = i;
+        i = i+1;
+        
         System.out.println(idade);
         System.out.println(idadeAnoQueVem);
         System.out.println(MetadeIdade);
         System.out.println(resultado);
+        System.out.println(TipoSangue);
+        System.out.println(maioridade);
+
+        
     }
 }
